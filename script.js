2b543656e847d20b1027e5884da1bfce32b91cba
Nazariy Lagoyda <35458441+Nazar1us@users.noreply.github.com>
2022-12-14 06:19:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// const closeButton = document.querySelector('.close-burger');
+// const openButton = document.querySelector('.open-burger');
+// const nav = document.querySelector('.nav');
+
+// closeButton.addEventListener("click", () => {
+//     nav.classList.remove('navigation-open');
+// }); 
+
+// openButton.addEventListener("click", () => {
+//     nav.classList.add('navigation-open');
+// }); 
